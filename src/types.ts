145c68ed8a172cdac6772c48cfbd85c0d08d3159
Castle <castlerock88@protonmail.com>
2022-11-12 18:14:56 -0500

    feat: project section

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type Frontmatter = {
+  layout: string;
+  title: string;
+  description: string;
+  image: {
+    url: string;
+    alt: string;
+  };
+  background: string;
+  tech: string[];
+};
